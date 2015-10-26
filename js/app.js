f3674caadaccd26b2b08736e4bcf80b9c1b480d0
nigirock <nigirock124@gmail.com>
2015-10-26 15:01:27 +0300

    bird problem

--- js/app.js
+++ js/app.js
@@ -3,18 +3,31 @@ var app = angular.module("APP",[]);
 app.controller("mainCtrl",function($scope,$http){
     $http.get("./js/data.json").success(function(data){
         $scope.city = data.city;
+        $scope.money = data.money;
         $scope.phone = $scope.city[2].phone;
+        $scope.name = $scope.money[0].name;
         $scope.activeClass = 'activeLi';
         $scope.textHotel = 'Simferopol Crimea Ukraine Train Station White Hostel';
         $scope.textHotel = $scope.textHotel.slice(0,45);
-        $scope.visible = {};
-        $scope.openPopup = false;
+        $scope.caretClass = false;
+        $scope.caretClass2 = false;
+        $scope.birdClass = "bird";
 
+        $scope.caretRevers = function(index){
+            $scope.caretClass = $scope.caretClass == false ? true : false;
+            $scope.money[index] = $scope.money[index].birdClass;
+            console.log($scope.money[index])
+        };
+        $scope.caretRevers2 = function(){
+            $scope.caretClass2 = $scope.caretClass2 == false ? true : false;
+        };
         $scope.addSpanPhone = function(phone){
             $scope.phone = phone;
         };
-    })
-    console.log();
+        $scope.addSpanName = function(name){
+            $scope.name = name;
+        };
+    });
 });
 $( document ).ready(function() {
 
